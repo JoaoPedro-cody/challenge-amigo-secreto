8cfe3963692a6973471f879a0ff9c7f94667869f
JoaoPedro-Cody <codyla2313@gmail.com>
2025-01-17 17:41:47 -0300

    Projeto Pronto

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nome = document.querySelector('input');
+let lista = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+
+
+function adicionarAmigo(){
+    if (nome.value.trim() === '' ){
+        alert('Por Favor, insira um nome');
+    }
+    else{
+        amigos.push(nome.value.trim());
+        nome.value = '';
+        atualizarAmigos();
+    }
+}
+
+function atualizarAmigos(){
+    lista.innerHTML = '';
+    for (let amigo of amigos){
+        const itemsLista = document.createElement('li');
+        itemsLista.textContent = amigo;
+        lista.appendChild(itemsLista);
+    }
+}
+
+function  sortearAmigo(){
+
+    if (amigos.length === 0 ){
+        resultado.innerHTML = 'O Sorteio de nomes acabou';
+    }
+    else{
+        let numeroSorteado = Math.floor(Math.random() * amigos.length);
+        lista.innerHTML = '';
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigos[numeroSorteado]}`;
+        console.log(numeroSorteado);
+
+        let amigoSorteado = amigos[numeroSorteado];
+        amigos.splice(numeroSorteado, 1);
+
+    }
+
+    console.log(amigos);
+}
+
